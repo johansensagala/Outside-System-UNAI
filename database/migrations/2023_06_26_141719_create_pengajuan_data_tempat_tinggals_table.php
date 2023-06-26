aee25b23b71db92f0b0bd588c1ddeb5b57835cc2
johansensagala <94831383+johansensagala@users.noreply.github.com>
2023-06-26 23:00:42 +0700

    Add files via upload
    adding migrations
    
    Signed-off-by: johansensagala <94831383+johansensagala@users.noreply.github.com>

---
+++ database/migrations/2023_06_26_141719_create_pengajuan_data_tempat_tinggals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pengajuan_data_tempat_tinggals', function (Blueprint $table) {
+            $table->id();
+            $table->string('alamat');
+            $table->string('gps');
+            $table->string('foto_tempat_tinggal');
+            $table->integer('kapasitas');
+            $table->string('persetujuan');
+            $table->unsignedBigInteger('id_penjamin');
+            $table->unsignedBigInteger('id_pr3');
+            $table->timestamps();
+
+            $table->foreign('id_penjamin')->references('id')->on('penjamins');
+            $table->foreign('id_pr3')->references('id')->on('biro_kemahasiswaans');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pengajuan_data_tempat_tinggals');
+    }
+};
---
+++ database/migrations/2023_06_26_141754_create_absensis_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('absensis', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal');
+            $table->string('hadir');
+            $table->unsignedBigInteger('id_mahasiswa');
+            $table->unsignedBigInteger('id_pegawai');
+            $table->timestamps();
+
+            $table->foreign('id_mahasiswa')->references('id')->on('mahasiswas');
+            $table->foreign('id_pegawai')->references('id')->on('biro_kemahasiswaans');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('absensis');
+    }
+};
