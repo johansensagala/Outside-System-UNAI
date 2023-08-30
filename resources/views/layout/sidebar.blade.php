164334e8a659eed1369713d6f61a23b123b11bd2
andrewsitohang <2081036@unai.edu>
2023-08-30 13:36:20 +0700

    membuat tampilan dashboard

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,314 @@
+<nav class="sidebar">
+  <div class="sidebar-header">
+    <a href="#" class="sidebar-brand">
+      Noble<span>UI</span>
+    </a>
+    <div class="sidebar-toggler not-active">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+  <div class="sidebar-body">
+    <ul class="nav">
+      <li class="nav-item nav-category">Main</li>
+      <li class="nav-item">
+        <a href="{{ url('/') }}" class="nav-link">
+          <i class="link-icon" data-feather="box"></i>
+          <span class="link-title">Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item nav-category">web apps</li>
+      <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#email" role="button" aria-controls="email">
+          <i class="link-icon" data-feather="mail"></i>
+          <span class="link-title">Email</span>
+          <i class="link-arrow" data-feather="chevron-down"></i>
+        </a>
+        <div class="collapse" id="email">
+          <ul class="nav sub-menu">
+            <li class="nav-item">
+              <a href="#" class="nav-link">Inbox</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Read</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Compose</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="link-icon" data-feather="message-square"></i>
+          <span class="link-title">Chat</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="link-icon" data-feather="calendar"></i>
+          <span class="link-title">Calendar</span>
+        </a>
+      </li>
+      <li class="nav-item nav-category">Components</li>
+      <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#uiComponents" role="button" aria-controls="uiComponents">
+          <i class="link-icon" data-feather="feather"></i>
+          <span class="link-title">UI Kit</span>
+          <i class="link-arrow" data-feather="chevron-down"></i>
+        </a>
+        <div class="collapse" id="uiComponents">
+          <ul class="nav sub-menu">
+            <li class="nav-item">
+              <a href="#" class="nav-link">Accordion</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Alerts</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Badges</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Breadcrumbs</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Buttons</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Button group</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Cards</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Carousel</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Collapse</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Dropdowns</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">List group</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Media object</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Modal</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Navs</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Navbar</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Pagination</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Popvers</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Progress</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Scrollbar</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Scrollspy</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Spinners</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Tabs</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Tooltips</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#advanced-ui" role="button" aria-controls="advanced-ui">
+          <i class="link-icon" data-feather="anchor"></i>
+          <span class="link-title">Advanced UI</span>
+          <i class="link-arrow" data-feather="chevron-down"></i>
+        </a>
+        <div class="collapse" id="advanced-ui">
+          <ul class="nav sub-menu">
+            <li class="nav-item">
+              <a href="#" class="nav-link">Cropper</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Owl Carousel</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">SortableJs</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Sweet Alert</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#forms" role="button" aria-controls="forms">
+          <i class="link-icon" data-feather="inbox"></i>
+          <span class="link-title">Forms</span>
+          <i class="link-arrow" data-feather="chevron-down"></i>
+        </a>
+        <div class="collapse" id="forms">
+          <ul class="nav sub-menu">
+            <li class="nav-item">
+              <a href="#" class="nav-link">Basic Elements</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Advanced Elements</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Editors</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Wizard</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#charts" role="button" aria-controls="charts">
+          <i class="link-icon" data-feather="pie-chart"></i>
+          <span class="link-title">Charts</span>
+          <i class="link-arrow" data-feather="chevron-down"></i>
+        </a>
+        <div class="collapse" id="charts">
+          <ul class="nav sub-menu">
+            <li class="nav-item">
+              <a href="#" class="nav-link">Apex</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">ChartJs</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Flot</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Peity</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Sparkline</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#tables" role="button" aria-controls="tables">
+          <i class="link-icon" data-feather="layout"></i>
+          <span class="link-title">Tables</span>
+          <i class="link-arrow" data-feather="chevron-down"></i>
+        </a>
+        <div class="collapse" id="tables">
+          <ul class="nav sub-menu">
+            <li class="nav-item">
+              <a href="#" class="nav-link">Basic Tables</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Data Table</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#icons" role="button" aria-controls="icons">
+          <i class="link-icon" data-feather="smile"></i>
+          <span class="link-title">Icons</span>
+          <i class="link-arrow" data-feather="chevron-down"></i>
+        </a>
+        <div class="collapse" id="icons">
+          <ul class="nav sub-menu">
+            <li class="nav-item">
+              <a href="#" class="nav-link">Feather Icons</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Mdi Icons</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item nav-category">Pages</li>
+      <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#general" role="button" aria-controls="general">
+          <i class="link-icon" data-feather="book"></i>
+          <span class="link-title">Special Pages</span>
+          <i class="link-arrow" data-feather="chevron-down"></i>
+        </a>
+        <div class="collapse" id="general">
+          <ul class="nav sub-menu">
+            <li class="nav-item">
+              <a href="#" class="nav-link">Blank page</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Faq</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Invoice</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Profile</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Pricing</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Timeline</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#auth" role="button" aria-controls="auth">
+          <i class="link-icon" data-feather="unlock"></i>
+          <span class="link-title">Authentication</span>
+          <i class="link-arrow" data-feather="chevron-down"></i>
+        </a>
+        <div class="collapse" id="auth">
+          <ul class="nav sub-menu">
+            <li class="nav-item">
+              <a href="#" class="nav-link">Login</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Register</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#error" role="button" aria-controls="error">
+          <i class="link-icon" data-feather="cloud-off"></i>
+          <span class="link-title">Error</span>
+          <i class="link-arrow" data-feather="chevron-down"></i>
+        </a>
+        <div class="collapse" id="error">
+          <ul class="nav sub-menu">
+            <li class="nav-item">
+              <a href="#" class="nav-link">404</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">500</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item nav-category">Docs</li>
+      <li class="nav-item">
+        <a href="https://www.nobleui.com/laravel/documentation/docs.html" target="_blank" class="nav-link">
+          <i class="link-icon" data-feather="hash"></i>
+          <span class="link-title">Documentation</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
