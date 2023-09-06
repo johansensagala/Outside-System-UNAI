c1b31e2256e868803a6c056caf095c76f4199381
johansensagala <johansensagala.2001@gmail.com>
2023-09-06 13:52:41 +0700

    tambah menu sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -10,24 +10,87 @@
   </div>
   <div class="sidebar-body" style="background-color: #38343c;">
     <ul class="nav">
+
       <li class="nav-item py-3">
         <a href="{{ url('/') }}" class="nav-link">
           <i class="link-icon" data-feather="table"></i>
           <span class="link-title">Dashboard</span>
         </a>
       </li>
+
+      {{-- MENU UNTUK PENJAMIN --}}
+
+      {{-- @if ($auth_user->role == 'penjamin') --}}
+
+      <li class="nav-item py-3">
+        <a href="{{ url('/') }}" class="nav-link">
+        <i class="link-icon" data-feather="user"></i>
+          <span class="link-title">Permohonan Tempat Tinggal</span>
+        </a>
+      </li>
+
+      <li class="nav-item py-3">
+        <a href="{{ url('/') }}" class="nav-link">
+        <i class="link-icon" data-feather="user"></i>
+          <span class="link-title">Persetujuan Mahasiswa</span>
+        </a>
+      </li>
+
+      {{-- MENU UNTUK MAHASISWA --}}
+
+      <li class="nav-item py-3">
+        <a href="{{ url('/') }}" class="nav-link">
+        <i class="link-icon" data-feather="user"></i>
+          <span class="link-title">Permohonan Penjamin</span>
+        </a>
+      </li>
+
+      <li class="nav-item py-3">
+        <a href="{{ url('/') }}" class="nav-link">
+        <i class="link-icon" data-feather="user"></i>
+          <span class="link-title">Permohonan Tinggal</span>
+        </a>
+      </li>
+
+      <li class="nav-item py-3">
+        <a href="{{ url('/') }}" class="nav-link">
+        <i class="link-icon" data-feather="user"></i>
+          <span class="link-title">Absensi</span>
+        </a>
+      </li>
+
+      {{-- MENU UNTUK MAHASISWA --}}
+
       <li class="nav-item py-3">
         <a href="{{ url('/') }}" class="nav-link">
         <i class="link-icon" data-feather="user"></i>
           <span class="link-title">Penjamin</span>
         </a>
       </li>
+
+      <li class="nav-item py-3">
+        <a href="{{ url('/') }}" class="nav-link">
+        <i class="link-icon" data-feather="user"></i>
+          <span class="link-title">Persetujuan Tempat Tinggal</span>
+        </a>
+      </li>
+
       <li class="nav-item py-3">
         <a href="{{ url('/') }}" class="nav-link">
           <i class="link-icon" data-feather="home"></i>
-          <span class="link-title">Data tempat tinggal</span>
+          <span class="link-title">Persetujuan Outside</span>
         </a>
       </li>
+
+      <li class="nav-item py-3">
+        <a href="{{ url('/') }}" class="nav-link">
+          <i class="link-icon" data-feather="home"></i>
+          <span class="link-title">Absensi Outside</span>
+        </a>
+      </li>
+
+      {{-- LOGOUT --}}
+
       <li class="nav-item py-3">
         <a href="{{ url('/') }}" class="nav-link">
           <i class="link-icon" data-feather="log-out"></i>
