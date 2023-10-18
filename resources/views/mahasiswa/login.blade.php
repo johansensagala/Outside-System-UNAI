2c849f94d75fd69f78874cffcca816e516f65272
Jacquelinejslstn <2081011@unai.edu>
2023-10-18 16:24:08 +0700

    Perubahan font dan rounded border

--- resources/views/mahasiswa/login.blade.php
+++ resources/views/mahasiswa/login.blade.php
@@ -8,13 +8,13 @@
 <div class="page-content d-flex justify-content-center" style="background-color: #32323a; min-height: 100vh;">
   <div class="row mx-0 auth-page">
     <div class="mx-auto" style="margin-top: 75px">
-      <div class="card mx-auto" style="border: none; width: 350px;">
+      <div class="card mx-auto" style="border: none; width: 350px; border-top-left-radius: 20px;border-top-right-radius: 20px">
         <div class="row">
           <div>
             <div style="border-radius: 10px" class="auth-form-wrapper justify-content-center">
-              <div class="text-center py-4" style="background-color: #38BBEB; border-bottom: 10px solid #2D9DC6">
+              <div class="text-center py-4" style="background-color: #38BBEB; border-bottom: 10px solid #2D9DC6; border-top-left-radius: 10px;border-top-right-radius: 10px">
                 <h4 class="d-block text-white">Universitas Advent Indonesia</h4>
-                <h5 class="fw-normal text-white">OUTSIDE SYSTEM FOR STUDENTS</h5>
+                <h5 class="fw-normal text-white" style="font-family: 'Open Sans',sans-serif;">Outside System for Students </h5>
               </div>
               <div>
                 @if (session()->has('success'))  
@@ -50,7 +50,7 @@
                       @enderror
                     </div>
                   </div>
-                  <div class="bg-white pt-3 pb-4">
+                  <div class="bg-white pt-3 pb-4" style="border-bottom-left-radius: 20px;border-bottom-right-radius: 20px">
                     <div class="form-check mb-3">
                       <a href="#" style="color: #1cb4ac; text-decoration: none;" class="link-underline-opacity-100-hover">
                           <small>
