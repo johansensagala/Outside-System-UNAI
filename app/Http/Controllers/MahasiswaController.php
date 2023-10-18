e5212c628b5c85e4e67f991209c9916137ab8351
Jacquelinejslstn <2081011@unai.edu>
2023-10-18 17:04:17 +0700

    Menghubungkan tampilan dashboard ke masing-masing role

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class MahasiswaController extends Controller
 {
+    public function index()
+    {
+        return view('mahasiswa.dashboard');
+    }
+
     public function showAbsensi () {
         return view('mahasiswa.absensi');
     }
