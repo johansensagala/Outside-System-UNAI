5ab6f90deac48e3969e370b5cddc1727f98b66b5
johansensagala <johansensagala.2001@gmail.com>
2023-10-22 23:22:15 +0700

    model permohonan penjaminan

--- app/Http/Controllers/DaftarPenjaminanMahasiswaController.php
+++ app/Http/Controllers/DaftarPenjaminanMahasiswaController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PengajuanPenjamin;
 use Illuminate\Http\Request;
 
 class DaftarPenjaminanMahasiswaController extends Controller
 {
     public function index()
     {
-        $daftar_data_penjamin = PengajuanDataPenjamin::get();
+        $daftar_data_mahasiswa = PengajuanPenjamin::get();
             
-        return view('biro_kemahasiswaan.daftar_penjamin', compact('daftar_data_penjamin'));
+        return view('penjamin.daftar_mahasiswa', compact('daftar_data_mahasiswa'));
     }
 }
