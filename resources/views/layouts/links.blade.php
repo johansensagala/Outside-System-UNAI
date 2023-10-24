8597671cd604463cdc2cce0c9aadc9c4af622ae4
Jacquelinejslstn <2081011@unai.edu>
2023-10-24 16:13:41 +0700

    Menghapus favicon dan menambahkan logo unai

--- resources/views/layouts/links.blade.php
+++ resources/views/layouts/links.blade.php
@@ -23,7 +23,7 @@
 <!-- CSRF Token -->
 <meta name="_token" content="{{ csrf_token() }}">
 
-<link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
+<link rel="shortcut icon" href="{{ asset('assets/images/UNAI_Logo.webp') }}">
 
 <!-- plugin css -->
 <link href="{{ asset('assets/fonts/feather-font/css/iconfont.css') }}" rel="stylesheet" />
