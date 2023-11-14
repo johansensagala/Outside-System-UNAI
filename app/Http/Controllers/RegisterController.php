1a03b80cc787b56d78b9204c42c64aecb20e4b77
andrewsitohang <2081036@unai.edu>
2023-11-14 15:49:18 +0700

    mengembangkan fitur OTP

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use App\Models\Penjamin;
-use Twilio\Rest\Client;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Cache;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
@@ -29,6 +30,9 @@ class RegisterController extends Controller
         $validatedData['password'] = Hash::make($validatedData['password']);
 
         $validatedData['otp'] = mt_rand(100000, 999999);
+        
+        $otpKey = 'otp_' . $validatedData['username'];
+        Cache::put($otpKey, $validatedData['otp'], now()->addMinutes(1)); // Adjust the expiration time as needed
 
         //store OTP in session
         $request->session()->put('username', $validatedData['username']);
@@ -45,18 +49,31 @@ class RegisterController extends Controller
     }
 
     private function sendOtpWhatsApp($nomor_telp, $otp) {
-        $account_sid = getenv("TWILIO_ACCOUNT_SID");
-        $auth_token = getenv("TWILIO_AUTH_TOKEN");
-        $twilio_number = getenv("TWILIO_WHATSAPP_NUMBER");
+        $curl = curl_init();
 
-        $client = new Client($account_sid, $auth_token);
-        $client->messages->create(
-            "whatsapp:".$nomor_telp,
-                [
-                   "from" => "whatsapp:".$twilio_number,
-                   "body" => "Hello, OTP $otp"
-                ]
-            );
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => getenv("FONNTE_LINK"),
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => array(
+        'target' => $nomor_telp,
+        'message' => 'Your OTP : '.$otp , 
+        'countryCode' => '62', //optional
+        ),
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: '.getenv("FONNTE_TOKEN") //change TOKEN to your actual token
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
     }
 
     public function showVerifyOtpForm(Request $request)
@@ -71,7 +88,10 @@ class RegisterController extends Controller
         $nomor_telp = $request->session()->get('nomor_telp');
         $otp = $request->session()->get('otp');
 
-        if($request->otp == $otp) {
+        $otpKey = 'otp_' . $username;
+        $cachedOtp = Cache::get($otpKey);
+
+        if($request->otp == $cachedOtp) {
             // OTP verification successful, proceed with user registration
             Penjamin::create([
                 'username' => $username,
@@ -79,8 +99,11 @@ class RegisterController extends Controller
                 'nama' => $nama,
                 'nomor_telp' => $nomor_telp,
             ]);
+            Cache::forget($otpKey);
+            
+            return redirect('/penjamin/login')->with('success', 'Registration successfull! Please login');
         } else {
-            return redirect('/login')->with('error', 'OTP tidak valid');
+            return redirect('/penjamin/register')->with('error', 'OTP tidak valid');
         }
     }
 }
