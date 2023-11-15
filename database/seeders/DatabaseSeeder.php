c439187a5c01ef90a41be05c7061eee613a71019
andrewsitohang <2081036@unai.edu>
2023-11-15 14:43:20 +0700

    memperbaiki migration dan seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,33 +21,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        Penjamin::create([
-            'username' => 'susi-susanti',
-            'password' => bcrypt('susi1234'),
-            'nama' => 'Susi Susanti',
-            'nomor_telp' => '085371655509',
-        ]);
-
-        Penjamin::create([
-            'username' => 'riama-aritonang',
-            'password' => bcrypt('riama123'),
-            'nama' => 'Riama Aritonang',
-            'nomor_telp' => '085398255509',
-        ]);
-
-        Penjamin::create([
-            'username' => 'fernando-sinaga',
-            'password' => bcrypt('fernando123'),
-            'nama' => 'Fernando Sinaga',
-            'nomor_telp' => '085371659776',
-        ]);
-
         Mahasiswa::create([
             'nim' => '2081031',
             'password' => bcrypt('johansen123'),
             'nama' => 'Johansen Sagala',
-            'semester' => '7',
-            'status' => 0,
+            'angkatan' => '2020',
             'nomor_pribadi' => '081323456789',
             'nomor_ortu_wali' => '081234567886',
         ]);
@@ -56,8 +34,7 @@ class DatabaseSeeder extends Seeder
             'nim' => '2081032',
             'password' => bcrypt('jonatan123'),
             'nama' => 'Jonatan Situmorang',
-            'semester' => '7',
-            'status' => 0,
+            'angkatan' => '2020',
             'nomor_pribadi' => '081334656789',
             'nomor_ortu_wali' => '081234346686',
         ]);
@@ -66,8 +43,7 @@ class DatabaseSeeder extends Seeder
             'nim' => '2081033',
             'password' => bcrypt('irpan123'),
             'nama' => 'Irpan Buri Siburian',
-            'semester' => '7',
-            'status' => 0,
+            'angkatan' => '2020',
             'nomor_pribadi' => '085267345679',
             'nomor_ortu_wali' => '081234569886',
         ]);
@@ -76,8 +52,7 @@ class DatabaseSeeder extends Seeder
             'nim' => '2081034',
             'password' => bcrypt('eli123'),
             'nama' => 'Eli Feri Josua Simatupang',
-            'semester' => '7',
-            'status' => 0,
+            'angkatan' => '2020',
             'nomor_pribadi' => '081323235789',
             'nomor_ortu_wali' => '081231234886',
         ]);
@@ -86,8 +61,7 @@ class DatabaseSeeder extends Seeder
             'nim' => '2081035',
             'password' => bcrypt('iman123'),
             'nama' => 'Iman Saputra Zendato',
-            'semester' => '7',
-            'status' => 0,
+            'angkatan' => '2020',
             'nomor_pribadi' => '081322343789',
             'nomor_ortu_wali' => '081673545678',
         ]);
@@ -96,8 +70,7 @@ class DatabaseSeeder extends Seeder
             'nim' => '2081036',
             'password' => bcrypt('krismes123'),
             'nama' => 'Krismes Situmeang',
-            'semester' => '7',
-            'status' => 0,
+            'angkatan' => '2020',
             'nomor_pribadi' => '081323456088',
             'nomor_ortu_wali' => '08117567886',
         ]);
@@ -106,12 +79,35 @@ class DatabaseSeeder extends Seeder
             'nim' => '2081037',
             'password' => bcrypt('perianto123'),
             'nama' => 'Perianto Sinaga',
-            'semester' => '7',
-            'status' => 0,
+            'angkatan' => '2020',
             'nomor_pribadi' => '081323456689',
             'nomor_ortu_wali' => '081237457886',
         ]);
 
+        Penjamin::create([
+            'username' => 'susi-susanti',
+            'password' => bcrypt('susi1234'),
+            'nama' => 'Susi Susanti',
+            'nomor_telp' => '085371655509',
+            'otp' => 509170
+        ]);
+
+        Penjamin::create([
+            'username' => 'riama-aritonang',
+            'password' => bcrypt('riama123'),
+            'nama' => 'Riama Aritonang',
+            'nomor_telp' => '085398255509',
+            'otp' => 874574
+        ]);
+        
+        Penjamin::create([
+            'username' => 'fernando-sinaga',
+            'password' => bcrypt('fernando123'),
+            'nama' => 'Fernando Sinaga',
+            'nomor_telp' => '085371659776',
+            'otp' => 131236
+        ]);
+        
         BiroKemahasiswaan::create([
             'username' => 'yunus-elon',
             'password' => bcrypt('yunus123'),
