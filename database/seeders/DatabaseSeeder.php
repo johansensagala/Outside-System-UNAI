2c5e3e8e501f4ff5ffe36d618997e78ed99eaa1c
andrewsitohang <2081036@unai.edu>
2023-11-15 14:53:20 +0700

    memperbaiki migration dan seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,69 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Mahasiswa::create([
+            'nim' => '2081031',
+            'password' => bcrypt('johansen123'),
+            'nama' => 'Johansen Sagala',
+            'angkatan' => '2020',
+            'nomor_pribadi' => '081323456789',
+            'nomor_ortu_wali' => '081234567886',
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2081032',
+            'password' => bcrypt('jonatan123'),
+            'nama' => 'Jonatan Situmorang',
+            'angkatan' => '2020',
+            'nomor_pribadi' => '081334656789',
+            'nomor_ortu_wali' => '081234346686',
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2081033',
+            'password' => bcrypt('irpan123'),
+            'nama' => 'Irpan Buri Siburian',
+            'angkatan' => '2020',
+            'nomor_pribadi' => '085267345679',
+            'nomor_ortu_wali' => '081234569886',
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2081034',
+            'password' => bcrypt('eli123'),
+            'nama' => 'Eli Feri Josua Simatupang',
+            'angkatan' => '2020',
+            'nomor_pribadi' => '081323235789',
+            'nomor_ortu_wali' => '081231234886',
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2081035',
+            'password' => bcrypt('iman123'),
+            'nama' => 'Iman Saputra Zendato',
+            'angkatan' => '2020',
+            'nomor_pribadi' => '081322343789',
+            'nomor_ortu_wali' => '081673545678',
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2081036',
+            'password' => bcrypt('krismes123'),
+            'nama' => 'Krismes Situmeang',
+            'angkatan' => '2020',
+            'nomor_pribadi' => '081323456088',
+            'nomor_ortu_wali' => '08117567886',
+        ]);
+
+        Mahasiswa::create([
+            'nim' => '2081037',
+            'password' => bcrypt('perianto123'),
+            'nama' => 'Perianto Sinaga',
+            'angkatan' => '2020',
+            'nomor_pribadi' => '081323456689',
+            'nomor_ortu_wali' => '081237457886',
+        ]);
+
         Penjamin::create([
             'username' => 'susi-susanti',
             'password' => bcrypt('susi1234'),
@@ -40,76 +103,6 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('fernando123'),
             'nama' => 'Fernando Sinaga',
             'nomor_telp' => '085371659776',
-        ]);
-
-        Mahasiswa::create([
-            'nim' => '2081031',
-            'password' => bcrypt('johansen123'),
-            'nama' => 'Johansen Sagala',
-            'semester' => '7',
-            'status' => 0,
-            'nomor_pribadi' => '081323456789',
-            'nomor_ortu_wali' => '081234567886',
-        ]);
-
-        Mahasiswa::create([
-            'nim' => '2081032',
-            'password' => bcrypt('jonatan123'),
-            'nama' => 'Jonatan Situmorang',
-            'semester' => '7',
-            'status' => 0,
-            'nomor_pribadi' => '081334656789',
-            'nomor_ortu_wali' => '081234346686',
-        ]);
-
-        Mahasiswa::create([
-            'nim' => '2081033',
-            'password' => bcrypt('irpan123'),
-            'nama' => 'Irpan Buri Siburian',
-            'semester' => '7',
-            'status' => 0,
-            'nomor_pribadi' => '085267345679',
-            'nomor_ortu_wali' => '081234569886',
-        ]);
-
-        Mahasiswa::create([
-            'nim' => '2081034',
-            'password' => bcrypt('eli123'),
-            'nama' => 'Eli Feri Josua Simatupang',
-            'semester' => '7',
-            'status' => 0,
-            'nomor_pribadi' => '081323235789',
-            'nomor_ortu_wali' => '081231234886',
-        ]);
-
-        Mahasiswa::create([
-            'nim' => '2081035',
-            'password' => bcrypt('iman123'),
-            'nama' => 'Iman Saputra Zendato',
-            'semester' => '7',
-            'status' => 0,
-            'nomor_pribadi' => '081322343789',
-            'nomor_ortu_wali' => '081673545678',
-        ]);
-
-        Mahasiswa::create([
-            'nim' => '2081036',
-            'password' => bcrypt('krismes123'),
-            'nama' => 'Krismes Situmeang',
-            'semester' => '7',
-            'status' => 0,
-            'nomor_pribadi' => '081323456088',
-            'nomor_ortu_wali' => '08117567886',
-        ]);
-
-        Mahasiswa::create([
-            'nim' => '2081037',
-            'password' => bcrypt('perianto123'),
-            'nama' => 'Perianto Sinaga',
-            'semester' => '7',
-            'status' => 0,
-            'nomor_pribadi' => '081323456689',
-            'nomor_ortu_wali' => '081237457886',
         ]);
 
         BiroKemahasiswaan::create([
