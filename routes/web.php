7d7b6fb613fac4725bc8d270fb66f536c97633f4
johansensagala <johansensagala.2001@gmail.com>
2023-11-22 16:51:58 +0700

    perbaiki double controller import di route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\AbsensiController;
 use App\Http\Controllers\PenjaminController;
-use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\LoginPenjaminController;
@@ -12,9 +12,7 @@ use App\Http\Controllers\LoginMahasiswaController;
 use App\Http\Controllers\FormulirPenjaminController;
 use App\Http\Controllers\BiroKemahasiswaanController;
 use App\Http\Controllers\AbsensiByMahasiswaController;
-use App\Http\Controllers\AbsensiByMahasiswaController;
 use App\Http\Controllers\PengajuanLuarAsramaController;
-use App\Http\Controllers\PersetujuanMahasiswaController;
 use App\Http\Controllers\PersetujuanMahasiswaController;
 use App\Http\Controllers\PersetujuanLuarAsramaController;
 use App\Http\Controllers\DataPengajuanMahasiswaController;
