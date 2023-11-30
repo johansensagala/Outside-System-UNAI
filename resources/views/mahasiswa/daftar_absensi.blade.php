9eb9174b08952f820a7ba83081e93fc12c40a80d
johansensagala <johansensagala.2001@gmail.com>
2023-12-01 00:13:31 +0700

    tambahkan kalender di daftar absensi mahasiswa

--- resources/views/mahasiswa/daftar_absensi.blade.php
+++ resources/views/mahasiswa/daftar_absensi.blade.php
@@ -20,7 +20,7 @@
                     </div>
                 </div>
                 <div class="card">
-                    <div class="m-5">
+                    <div class="m-5 row">
                         <div class="table-responsive col-md-8 mt-3">
                             <table class="table table-bordered table-striped">
                                 <thead>
@@ -71,9 +71,29 @@
                             <div class="card mb-3">
                                 <div class="card-body">
                                     <h5 class="card-title text-center">Tanggal</h5>
-
-                                    <input type="text" id="tanggal-input" class="form-control" placeholder="Pilih Tanggal" />
-                        
+                                    <label for="jenisFilter" class="mb-1">Pilih Filter Tanggal</label>
+                                    <select id="jenisFilter" class="form-select mb-4" aria-label="Default select example">
+                                        <option>Berdasarkan tanggal</option>
+                                        <option>Berdasarkan interval tanggal</option>
+                                    </select>
+                            
+                                    <div id="tanggal" class="">
+                                        <label for="tanggalInput" class="mb-1">Pilih Filter Tanggal</label>
+                                        <input type="text" id="tanggalInput" class="form-control mb-2" placeholder="Pilih Tanggal" value="2023-08-17"/>
+                                        <button class="btn btn-primary mb-3" id="simpanButton1">
+                                            Tetapkan
+                                        </button>    
+                                    </div>
+                                    
+                                    <div id="intervalTanggal" class="d-none">
+                                        <label for="tanggalAwal" class="mb-1">Pilih Tanggal Awal</label>
+                                        <input type="text" id="tanggalAwal" class="form-control mb-2" placeholder="Pilih Tanggal"/>
+                                        <label for="tanggalAkhir" class="mb-1">Pilih Tanggal Akhir</label>
+                                        <input type="text" id="tanggalAkhir" class="form-control mb-2" placeholder="Pilih Tanggal"/>
+                                        <button class="btn btn-primary" id="simpanButton2">
+                                            Tetapkan
+                                        </button>    
+                                    </div>                                                            
                                 </div>
                             </div>
                             <div class="card mb-3">
@@ -90,10 +110,38 @@
 
 <script>
 document.addEventListener('DOMContentLoaded', function () {
-    flatpickr("#tanggal-input", {
+    // Untuk membuat kalender
+
+    flatpickr("#tanggalInput", {
         enableTime: false,
         dateFormat: "Y-m-d",
     });
+
+    flatpickr("#tanggalAwal", {
+        enableTime: false,
+        dateFormat: "Y-m-d",
+    });
+
+    flatpickr("#tanggalAkhir", {
+        enableTime: false,
+        dateFormat: "Y-m-d",
+    });
+
+    // Untuk select jenis filter
+
+    document.getElementById('jenisFilter').addEventListener('change', function () {
+        let selectedOption = this.value;
+
+        document.getElementById('tanggal').classList.add('d-none');
+        document.getElementById('intervalTanggal').classList.add('d-none');
+
+        if (selectedOption === 'Berdasarkan tanggal') {
+            document.getElementById('tanggal').classList.remove('d-none');
+        } else if (selectedOption === 'Berdasarkan interval tanggal') {
+            document.getElementById('intervalTanggal').classList.remove('d-none');
+        }
+    });
+
 });
 </script>
 
