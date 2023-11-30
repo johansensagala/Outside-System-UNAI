f6ea0b5657574b31eb326e965dea649519bb72c0
johansensagala <johansensagala.2001@gmail.com>
2023-11-30 23:41:22 +0700

    tambah plugin flatpickr

--- resources/views/mahasiswa/daftar_absensi.blade.php
+++ resources/views/mahasiswa/daftar_absensi.blade.php
@@ -21,24 +21,7 @@
                 </div>
                 <div class="card">
                     <div class="m-5">
-                        {{-- <div class="row">
-                            <div class="col-10">
-                                <form action="/biro/formulir-penjamin">
-                                    <div class="input-group mb-3">
-                                        <!-- <input type="text" class="form-control" placeholder="Masukkan nama penjamin..." name="search" id="search" value="{{ request('search') }}"> -->
-                                        <input type="text" class="form-control" placeholder="NIM atau Nama Mahasiswa..." name="search" id="search">
-                                    </div>
-                                </form>
-                            </div>
-                            <div class="col-2">
-                                <select name="status" id="status" class="form-control form-select">
-                                    <option value="">Status Absensi</option>
-                                    <option value="Hadir">Hadir</option>
-                                    <option value="Absen">Absen</option>
-                                </select>
-                            </div>
-                        </div> --}}
-                        <div class="table-responsive">
+                        <div class="table-responsive col-md-8 mt-3">
                             <table class="table table-bordered table-striped">
                                 <thead>
                                     <tr>
@@ -59,7 +42,7 @@
                                         <td class="align-middle">{{ $index }}</td>
                                         <td class="align-middle">{{ $absen->mahasiswa->nim }}</td>
                                         <td class="align-middle">{{ $absen->mahasiswa->nama }}</td>
-                                        <td class="align-middle"><button type="button" class="btn btn-primary" onclick="window.location.href='/mhs/daftar-absensi/{{ $absen->id }}'">Detail</button></td>
+                                        <td class="align-middle"><button type="button" class="btn btn-primary" onclick="window.location.href='/mhs/daftar-absensi-mahasiswa/{{ $absen->id }}'">Detail</button></td>
                                         <td class="align-middle">{{ $absen->created_at }}</td>
                                         <td class="align-middle">
                                             @if ($absen->kehadiran == 'Hadir')
@@ -84,46 +67,34 @@
                                 </tbody>
                             </table>
                         </div>
+                        <div class="col-md-4 mt-3">
+                            <div class="card mb-3">
+                                <div class="card-body">
+                                    <h5 class="card-title text-center">Tanggal</h5>
+
+                                    <input type="text" id="tanggal-input" class="form-control" placeholder="Pilih Tanggal" />
+                        
+                                </div>
+                            </div>
+                            <div class="card mb-3">
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
                 
             </div>
         </div>
     </div>
-
-    <!-- <div class="col-md-4 grid-margin">
-        <div class="card">
-            <div class="card-header text-center">
-                Tanggal         
-    <div class="col-md-4 grid-margin">
-                <div class="card">
-                    <div class="card-header text-center">
-                        Status Permohonan Tempat Tinggal
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">                            
-                            <div class="bg-success p-2 rounded-3 text-white text-center">
-                                Disetujui
-                            </div>
-
-                            
-                            <div class="bg-danger p-2 rounded-3 text-white text-center">
-                                Ditolak
-                            </div>
-
-
-                            <div class="bg-warning p-2 rounded-3 text-white text-center">
-                                Menunggu Persetujuan
-                            </div>
-                            
-                        </li>
-                    </ul>
-                    <input type="date" class="form-control" id="tanggal_absensi" name="tanggal_absensi">
-                </div>
-            </div>
-            <input type="date" class="form-control" id="tanggal_absensi" name="tanggal_absensi">
-        </div>
-    </div> -->
 </div>
+
+<script>
+document.addEventListener('DOMContentLoaded', function () {
+    flatpickr("#tanggal-input", {
+        enableTime: false,
+        dateFormat: "Y-m-d",
+    });
+});
+</script>
 
 @endsection
